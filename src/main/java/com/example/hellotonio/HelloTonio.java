1c811d8d5fd4d2ab1c6da18d253e231473b5b0f2
Tonio Caputo <caputo.e@opendevpro.com>
2024-05-02 12:37:56 -0300

    Add name RestService

--- src/main/java/com/example/hellotonio/HelloTonio.java
+++ src/main/java/com/example/hellotonio/HelloTonio.java
@@ -3,6 +3,7 @@ package com.example.hellotonio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,7 +14,13 @@ public class HelloTonio {
     public String home() {
         return "Hello Tonio!";
     }
-	public static void main(final String[] args) {
+
+    @GetMapping("/name/{name}")
+    public String name(@PathVariable final String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    public static void main(final String[] args) {
 		SpringApplication.run(HelloTonio.class, args);
 	}
 
