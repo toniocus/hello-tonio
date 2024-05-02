8cf140829589267ae38830fa71475657dd3c4af3
Tonio Caputo <caputo.e@opendevpro.com>
2024-05-02 11:44:43 -0300

    init project

---
+++ src/main/java/com/example/hellotonio/HelloTonio.java
@@ -0,0 +1,13 @@
+package com.example.hellotonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTonio {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTonio.class, args);
+	}
+
+}
